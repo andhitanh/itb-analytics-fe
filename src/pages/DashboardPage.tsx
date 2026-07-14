c83452331da0ed97921415442287ef4d49dd8132
andhitaan <terestrea12@gmail.com>
2026-07-14 16:05:42 +0700

    fix : clean up wisudawan

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,11 @@
 import { useDataMode }        from '@/context/DataModeContext';
 import DashboardAkademik  from '@/features/dashboard/DashboardAkademik';
-import DashboardWisudawan from '@/features/dashboard/DashboardWisudawan';
+// import DashboardWisudawan from '@/features/dashboard/DashboardWisudawan';
 
 export default function DashboardPage() {
   const { dataMode } = useDataMode();
 
   return dataMode === 'wisudawan'
-    ? <DashboardWisudawan />
+    ? <DashboardAkademik />
     : <DashboardAkademik />;
 }
