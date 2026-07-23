186315c818e3a07cbac5b489649b9dc17e8d1e1e
andhitaan <terestrea12@gmail.com>
2026-07-23 16:48:20 +0700

    fix : desc instruction

--- src/features/dashboard/components/dosen/TabPenilaianDosen.tsx
+++ src/features/dashboard/components/dosen/TabPenilaianDosen.tsx
@@ -40,8 +40,8 @@ export function TabPenilaianDosen({ filter }: TabPenilaianDosenProps) {
 
       <Card>
         <CardHeader>
-          <CardTitle>Rata-Rata Hasil Kuesioner Mahasiswa per Kategori</CardTitle>
-          <CardDescription>Klik 2x salah satu kategori untuk melihat rincian nilai per pertanyaan kuesioner</CardDescription>
+          <CardTitle>Rata-Rata Hasil Kuesioner Mahasiswa per Kategori Pertanyaan</CardTitle>
+          <CardDescription>Klik salah satu kategori untuk melihat rincian nilai per pertanyaan kuesioner</CardDescription>
         </CardHeader>
         <CardContent>
           {kategoriLoading ? (
